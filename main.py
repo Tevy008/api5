861ccee5a35a54c45dd32c1e407cdea3bb9aec1b
Tevy008 <lkolla21@bk.ru>
2024-03-25 16:14:44 +0300

    Добавил изменения в код

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import os
 
 def predict_rub_salary(salary_from = None,salary_to = None):
     if salary_from and salary_to:
-        everage_sallary = int((salary_from+salary_to)/2)
+        average_salary = int((salary_from+salary_to)/2)
     elif salary_from:
-        everage_sallary = int(salary_from * 1.2)
+        average_salary = int(salary_from * 1.2)
     elif salary_to:
-        everage_sallary = int(salary_to * 0.8)
+        average_salary = int(salary_to * 0.8)
     else:
-        everage_sallary = None
-    return everage_sallary
+        average_salary = None
+    return average_salary
 
 
 def get_vacancies_hh(language, page=0):
@@ -31,32 +31,33 @@ def get_vacancies_hh(language, page=0):
 
 
 def get_statistic_hh():
-    found_vacancy = {}
+    vacancies_found = {}
     langs = [
-    "python","java",
+        "python",
+        "java",
     ]
     for lang in langs:
-        salary_vacancyes=[]
+        salary_vacancies=[]
         for page in count(0):
-            vacancyes = get_vacancies_hh(lang, page=page)
-            if page >= vacancyes['pages']-1:
+            vacancies = get_vacancies_hh(lang, page=page)
+            if page >= vacancies['pages']-1:
                 break
-            for vacancy in vacancyes["items"]:
+            for vacancy in vacancies["items"]:
                 salary = vacancy.get("salary")
                 if salary and salary["currency"] == "RUR":
-                    get_predict_salary = predict_rub_salary(vacancy["salary"].get("from"),vacancy["salary"].get("to"))
-                if get_predict_salary:
-                    salary_vacancyes.append(get_predict_salary)
-        everage_salary = None
-        if salary_vacancyes:
-            everage_salary = int(sum(salary_vacancyes)/len(salary_vacancyes)) 
+                    predicted_salary = predict_rub_salary(vacancy["salary"].get("from"),vacancy["salary"].get("to"))
+                if predicted_salary:
+                    salary_vacancies.append(predicted_salary)
+        average_salary = None
+        if salary_vacancies:
+            average_salary = int(sum(salary_vacancies)/len(salary_vacancies)) 
             
-        found_vacancy[lang] = {
-            "vacancies_found": vacancyes["found"],
-            "vacancies_processed": len(salary_vacancyes),
-            "average_salary": everage_salary,
+        vacancies_found[lang] = {
+            "vacancies_found": vacancies["found"],
+            "vacancies_processed": len(salary_vacancies),
+            "average_salary": average_salary,
         }
-    return found_vacancy
+    return vacancies_found
 
 
 def get_vacancies_sj(sj_key, lang, page=0): 
@@ -77,38 +78,45 @@ def get_vacancies_sj(sj_key, lang, page=0):
 
     
 def predict_rub_salary_for_superJob(sj_key):
-    found_vacancy = {}
+    vacancies_found = {}
     langs = [
-    "python","java","javascript","ruby","php","c++","c#","C",
-   ]
+        "python",
+        "java",
+        "javascript",
+        "ruby",
+        "php",
+        "c++",
+        "c#",
+        "C",
+    ]
     for lang in langs:
-        salary_vacancyes=[]
+        salary_vacancies=[]
         for page in count(0,1):
-            vacancyes = get_vacancies_sj(sj_key, lang, page=page,)
-            if page in vacancyes['objects']:
+            vacancies = get_vacancies_sj(sj_key, lang, page=page,)
+            if not vacancies['objects']:
                 break
-            for vacancy in vacancyes["objects"]:
-                get_predict_salary = predict_rub_salary(vacancy["payment_from"],vacancy["payment_to"])
-                if get_predict_salary:
-                    salary_vacancyes.append(get_predict_salary)
-        everage_salary = None
-        if salary_vacancyes:
-            everage_salary = int(sum(salary_vacancyes)/len(salary_vacancyes)) 
+            for vacancy in vacancies["objects"]:
+                predicted_salary = predict_rub_salary(vacancy["payment_from"],vacancy["payment_to"])
+                if predicted_salary:
+                    salary_vacancies.append(predicted_salary)
+        average_salary = None
+        if salary_vacancies:
+            average_salary = int(sum(salary_vacancies)/len(salary_vacancies)) 
             
-        found_vacancy[lang] = {
-            "vacancies_found": vacancyes["found"],
-            "vacancies_processed": len(salary_vacancyes),
-            "average_salary": everage_salary,
+        vacancies_found[lang] = {
+            "vacancies_found": vacancies["found"],
+            "vacancies_processed": len(salary_vacancies),
+            "average_salary": average_salary,
         }
-    return found_vacancy
+    return vacancies_found
 
 
 def  create_table(title,statistics):
     table_data = [
         ["Язык программирования", "Вакансий найдено", "Вакансий обработано", "Средняя зарплата"]
     ]
-    for lang, vacancyes in statistics.items():
-        table_data.append([lang,vacancyes["vacancies_found"],vacancyes["vacancies_processed"],vacancyes["average_salary"]])
+    for lang, vacancies in statistics.items():
+        table_data.append([lang,vacancies["vacancies_found"],vacancies["vacancies_processed"],vacancies["average_salary"]])
     table = AsciiTable(table_data, title)
     return table.table
 
